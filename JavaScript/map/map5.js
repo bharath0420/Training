091d5a629d66f03039220e861189548efe9b6afa
bharath0420 <bharath.trl@gmail.com>
2023-09-05 16:14:07 +0530

    cc

---
+++ JavaScript/map/map5.js
@@ -0,0 +1,26 @@
+/* Write a function that takes an array of objects with a category property and returns an object where the keys are the categories, and 
+the values are arrays of objects with that category. Use map  */
+s=[
+    {
+      'name':"tomato",
+      'category':'veg'
+    },
+    {
+      'name':"apple",
+      'category':'fruit'
+    },
+    {
+      'name':"cucumber",
+      'category':'veg'
+    },
+    {
+      'name':"broccoli",
+      'category':'veg'
+    },
+    {
+      'name':"watermelon",
+      'category':'fruit'
+    }
+  ]
+  function category(s)
+  
